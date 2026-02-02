c48592d0669decd24a6d43b11f5b083120e57a4e
arrowaydigital <arrowaydigital@gmail.com>
2026-02-02 02:20:50 -0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { HeroSection } from "@/components/features/HeroSection";
 import { HomeServicesPreview } from "@/components/features/HomeServicesPreview";
 import { HomeMetrics } from "@/components/features/HomeMetrics";
 import { HomeTestimonial } from "@/components/features/HomeTestimonial";
-import { HomeCTA } from "@/components/features/HomeCTA";
+import { CaseStudiesSection } from "@/components/features/CaseStudiesSection";
+import { ProcessSection } from "@/components/features/ProcessSection";
+
 import { PageTransition } from "@/components/animations/PageTransition";
 
 export function HomePage() {
@@ -15,9 +18,10 @@ export function HomePage() {
         <main>
           <HeroSection />
           <HomeServicesPreview />
+          <CaseStudiesSection />
+          <ProcessSection />
           <HomeMetrics />
           <HomeTestimonial />
-          <HomeCTA />
         </main>
         <Footer />
       </div>
